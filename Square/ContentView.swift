c59583b75bcb35217b6e63419ca2333c286c81fd
Arryn01 <arrynlyness@gmail.com>
2023-04-23 01:36:07 +0100

    Initial Commit

--- Square/ContentView.swift
+++ Square/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, no one!")
+            Text("Hello, someone!")
         }
         .padding()
     }
