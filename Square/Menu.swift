2c2892606f728af4c716df52e8d44b4ae260156e
Arryn01 <arrynlyness@gmail.com>
2023-04-23 16:43:49 +0100

    Initial Commit

--- Square/Menu.swift
+++ Square/Menu.swift
@@ -10,58 +10,148 @@ import SwiftUI
 struct MenuView: View {
     var body: some View {
         ZStack {
+            
+            
             Rectangle()
                 .frame(height: 50)
                 .padding(.top, 50)
                 .background(Color(#colorLiteral(red: 0.9843137255, green: 0.6901960784, blue: 0.2352941176, alpha: 1)))
                 .foregroundColor(Color(#colorLiteral(red: 0.9843137255, green: 0.6901960784, blue: 0.2352941176, alpha: 1)))
-            
                 .cornerRadius(20)
             
-            Text("Truth OR Dare")
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .padding(/*@START_MENU_TOKEN@*/.top, 50.0/*@END_MENU_TOKEN@*/)
+            ZStack {
+                Text("Truth Or Dare")
+                    .font(.custom("FredokaOne-Regular", size: 36))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 50)
                 
                 
+            }
             
-        
+            
+            
         }
         
+        
+        
+        VStack(spacing: 25) {
+            
+            Spacer()
+            
+            ZStack {
+            
+            Rectangle()
+                .frame(width: 300, height: 25)
+                .padding(.top, 50)
+                .background(Color.green)
+                .foregroundColor(Color.green)
+                .cornerRadius(20)
+                
+                Text("Test")
+                    .foregroundColor(Color.white)
+                    .font(.custom("FredokaOne-Regular", size: 24))
+                
+            }
+            
+            ZStack {
+                
+                Rectangle()
+                    .frame(width: 300, height: 25)
+                    .padding(.top, 50)
+                    .background(Color(#colorLiteral(red: 0.6901960784, green: 0.8235294118, blue: 0.9254901961, alpha: 1))
+                    )
+                    .foregroundColor(Color(#colorLiteral(red: 0.6901960784, green: 0.8235294118, blue: 0.9254901961, alpha: 1))
+                    )
+                    .cornerRadius(20)
+                
+                Text("Test")
+                    .foregroundColor(Color.white)
+                    .font(.custom("FredokaOne-Regular", size: 24))
+                
+            }
+            
+            ZStack {
+                
+                Rectangle()
+                    .frame(width: 300, height: 25)
+                    .padding(.top, 50)
+                    .background(Color.teal)
+                    .foregroundColor(Color.teal)
+                    .cornerRadius(20)
+                
+                Text("Test")
+                    .foregroundColor(Color.white)
+                    .font(.custom("FredokaOne-Regular", size: 24))
+                
+            }
+            
+            ZStack {
+                
+                Rectangle()
+                    .frame(width: 300, height: 25)
+                    .padding(.top, 50)
+                    .background(Color.cyan)
+                    .foregroundColor(Color.cyan)
+                    .cornerRadius(20)
+                
+                Text("Test")
+                    .foregroundColor(Color.white)
+                    .font(.custom("FredokaOne-Regular", size: 24))
+                
+            }
+            
+            ZStack {
+                
+                Rectangle()
+                    .frame(width: 300, height: 25)
+                    .padding(.top, 50)
+                    .background(Color.blue)
+                    .foregroundColor(Color.blue)
+                    .cornerRadius(20)
+                
+                Text("Test")
+                    .foregroundColor(Color.white)
+                    .font(.custom("FredokaOne-Regular", size: 24))
+                
+            }
+            
+        }
         
         
         
         
         VStack {
-                    Spacer()
-                    Rectangle()
-                        .frame(height: 50)
-                        .padding(.bottom, 50)
-                        .background(Color(#colorLiteral(red: 0.9843137255, green: 0.6901960784, blue: 0.2352941176, alpha: 1)))
-                        .foregroundColor(Color(#colorLiteral(red: 0.9843137255, green: 0.6901960784, blue: 0.2352941176, alpha: 1)))
-                        .cornerRadius(20)
-                }
-        
-    }
-}
-
-
-struct Menu: View {
-    var body: some View {
-        VStack(spacing: 0) {
-            MenuView()
-                .ignoresSafeArea()
-
+            Spacer()
+            Rectangle()
+                .frame(height: 50)
+                .padding(.bottom, 50)
+                .background(Color(#colorLiteral(red: 0.9843137255, green: 0.6901960784, blue: 0.2352941176, alpha: 1)))
+                .foregroundColor(Color(#colorLiteral(red: 0.9843137255, green: 0.6901960784, blue: 0.2352941176, alpha: 1)))
+                .cornerRadius(20)
             
+
+            }
         }
     }
-}
-
-
-struct Menu_Previews: PreviewProvider {
-    static var previews: some View {
-        Menu()
+    
+    
+    
+    
+    struct Menu: View {
+        var body: some View {
+            VStack(spacing: 0) {
+                MenuView()
+                    .ignoresSafeArea()
+                
+                
+            }
+        }
     }
-}
+    
+    
+    struct Menu_Previews: PreviewProvider {
+        static var previews: some View {
+            Menu()
+        }
+    }
