7347171a96020f66783768e4b5fe375f000119c7
manmatter1 <81937141+manmatter1@users.noreply.github.com>
2023-04-26 19:32:13 +0100

    Update ThePrompt.swift

--- Square/Views/ThePrompt.swift
+++ Square/Views/ThePrompt.swift
@@ -1,10 +1,15 @@
 import SwiftUI
 
 struct ThePrompt: View {
+    
+
     // Define variables to hold the current truth and dare prompts
     
     @State var thePrompt = "Truth or Dare"
-    @State var istruth = true
+    @State var isTruth = true
+    @State var buttonPressed = false
+    
+    @State private var feedback = UINotificationFeedbackGenerator()
     
     // efine a variable to hold the category caught
     @State var categoryCaught:String
@@ -43,86 +48,109 @@ struct ThePrompt: View {
     }
     
     var body: some View {
-        VStack {
-            Spacer()
+        
+        ZStack {
+            BackgroundGradient()
+             .ignoresSafeArea()
             
-            ZStack {
-                if thePrompt == "Truth or Dare" {
-                    AnimatedGradientRectangle()
-                    .cornerRadius(15)
-                    .frame(width: .infinity,height: .infinity)
-                    .padding(.bottom)
-                    .padding(20)
-                } else{
-                    
-                    Rectangle()
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    if thePrompt == "Truth or Dare" {
+                        AnimatedGradientRectangle()
                         .cornerRadius(15)
-                        .foregroundColor(gradientColor()) // Set the color using a gradient
                         .frame(width: .infinity,height: .infinity)
                         .padding(.bottom)
                         .padding(20)
+                    } else{
+                        
+                        Rectangle()
+                            .cornerRadius(15)
+                            .foregroundColor(gradientColor())
+                            .frame(width: .infinity,height: .infinity)
+                            .padding(.bottom)
+                            .padding(20)
+                            .shadow(radius: 20)
+                            .glow(color: Color.yellow)
+                        
+                    }
+                    
+                        
+                    
+                
+                    
+                    Text("\(thePrompt)")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10.0)
+                        .foregroundColor(.white)
+                        .padding()
+                        .id(thePrompt) // Add an id modifier to detect changes to thePrompt
+                        .transition(.opacity)
+                        .fontWeight(.medium)
+                    
+                      
+                        
+                        
+                    
                 }
+                .padding(.bottom, 200.0)
                 
-            
                 
-                Text("\(thePrompt)")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 10.0)
-                    .foregroundColor(.white)
-                    .padding()
-                    .id(thePrompt) // Add an id modifier to detect changes to thePrompt
-                    .transition(.opacity)
-                  
+                
+                
+                
+                Spacer()
+                
+                //Buttons
+                HStack{
+                    Spacer()
+                    Button(action: {
+                        feedback.notificationOccurred(.success)
+                        //change colour
+                        isTruth = true
+                        // Load the JSON data and find a random truth prompt from the chosen category
+                        let prompts = loadJSONData()
+                        thePrompt = getRandomTruthPrompt(from: categoryCaught, prompts: prompts)
+                    }, label: {
+                        Text("Truth")
+                            .frame(width: 150,height: 75)
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .shadow(radius: 20)
+                        
+                    })
+                    
+                    Spacer()
+                    
+          
                     
                     
+                    Button(action: {
+                        feedback.notificationOccurred(.success)
+                        //Chnage colour
+                        isTruth = false
+                        // Load the JSON data and find a random dare prompt from the chosen category
+                        let prompts = loadJSONData()
+                        thePrompt = getRandomDarePrompt(from: categoryCaught, prompts: prompts)
+                    }, label: {
+                        Text("Dare")
+                            .frame(width: 150,height: 75)
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .shadow(radius: 20)
+                        
+                    })
+                    Spacer()
+                }
+                .padding(.bottom, 100.0)
                 
             }
-            .padding(.bottom, 200.0)
-            
-            Spacer()
-            
-            //Buttons
-            HStack{
-                Spacer()
-                Button(action: {
-                    //change colour
-                    istruth = true
-                    // Load the JSON data and find a random truth prompt from the chosen category
-                    let prompts = loadJSONData()
-                    thePrompt = getRandomTruthPrompt(from: categoryCaught, prompts: prompts)
-                }, label: {
-                    Text("Truth")
-                        .frame(width: 150,height: 75)
-                        .font(.title2)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                    
-                })
-                
-                Spacer()
-                
-                
-                Button(action: {
-                    //Chnage colour
-                    istruth = false
-                    // Load the JSON data and find a random dare prompt from the chosen category
-                    let prompts = loadJSONData()
-                    thePrompt = getRandomDarePrompt(from: categoryCaught, prompts: prompts)
-                }, label: {
-                    Text("Dare")
-                        .frame(width: 150,height: 75)
-                        .font(.title2)
-                        .foregroundColor(.white)
-                        .background(Color.red)
-                        .cornerRadius(10)
-                    
-                })
-                Spacer()
-            }
-            .padding(.bottom, 100.0)
-            
         }
     }
     
@@ -131,7 +159,7 @@ struct ThePrompt: View {
         if thePrompt == "Truth or Dare" {
             // Use a gradient with blue and red colors
             return Color.orange.opacity(0.5) // Use only one color with opacity
-        } else if istruth {
+        } else if isTruth {
             // Use blue color
             return Color.blue
         } else {
@@ -144,7 +172,7 @@ struct ThePrompt: View {
 }
 struct ThePrompt_Previews: PreviewProvider {
     static var previews: some View {
-        ThePrompt(categoryCaught: "Kids")
+        ThePrompt(categoryCaught: "Teens")
     }
 }
 
@@ -167,6 +195,28 @@ struct AnimatedGradientRectangle: View {
     }
 }
 
+struct BackgroundGradient: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State private var gradientStart = UnitPoint(x: 0, y: 0)
+    @State private var gradientEnd = UnitPoint(x: 1, y: 1)
+    
+    private var gradient: Gradient {
+        let colors: [Color] = [.orange, colorScheme == .dark ? .red : .pink]
+        return Gradient(colors: colors)
+    }
+    
+    var body: some View {
+        Rectangle()
+            .fill(LinearGradient(gradient: gradient, startPoint: gradientStart, endPoint: gradientEnd))
+            .frame(width: .infinity, height: .infinity)
+            .cornerRadius(15)
+            .animation(Animation.linear(duration: 2).repeatForever(autoreverses: true))
+            .onAppear {
+                gradientStart = UnitPoint(x: 1, y: 0)
+                gradientEnd = UnitPoint(x: 0, y: 1)
+            }
+    }
+}
 
 
 // Define a struct to represent a prompt in the JSON data
@@ -176,3 +226,31 @@ struct Prompt: Codable {
     let category: String
     let value: Int
 }
+
+//Class for if it is Truth or not
+
+class isTruthest: ObservableObject{
+    @Published var isTruth = true
+}
+
+//glow efffect
+
+struct Glow: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        ZStack{
+            content
+                .blur(radius: 50)
+                .shadow(color: color, radius: 20)
+            content
+        }
+    }
+}
+
+extension View{
+    func glow(color: Color = .white) -> some View{
+        modifier(Glow(color: color))
+    }
+}
+
